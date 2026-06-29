f1882c9692b58bee5702ebc3e4f43e978628adf0
Vidar Magnusson <git@vidarmagnusson.com>
2026-06-29 13:56:26 +0200

    Fix host_setup page

--- packages/web/src/views/host_setup.rs
+++ packages/web/src/views/host_setup.rs
@@ -1,12 +1,14 @@
+use api::endpoints::game::CreateGameResponse;
 use dioxus::prelude::*;
-
-use crate::Route;
 
 #[component]
 pub fn HostSetup() -> Element {
     let nav = use_navigator();
     rsx! {
         ui::host_setup::HostSetupForm {
+            on_created: move |resp: CreateGameResponse| {
+                let game_id = resp.game_id.to_string();
+            },
             // on_created: move |resp: api::models::CreateGameResponse| {
             //     let game_id = resp.game_id.to_string();
             //     // let route = if resp.role == api::models::TeamRole::Seeker {
