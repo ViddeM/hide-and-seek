f3be5532175396b00f04616dccc12003d2d566b4
Vidar Magnusson <git@vidarmagnusson.com>
2026-06-29 15:32:20 +0200

    Refactor TODO code in host setup

--- packages/web/src/views/host_setup.rs
+++ packages/web/src/views/host_setup.rs
@@ -8,16 +8,8 @@ pub fn HostSetup() -> Element {
         ui::host_setup::HostSetupForm {
             on_created: move |resp: CreateGameResponse| {
                 let game_id = resp.game_id.to_string();
+                // nav.push(Route::GameView { game_id });
             },
-            // on_created: move |resp: api::models::CreateGameResponse| {
-            //     let game_id = resp.game_id.to_string();
-            //     // let route = if resp.role == api::models::TeamRole::Seeker {
-            //     //     // Route::SeekerView { game_id }
-            //     // } else {
-            //     //     // Route::HiderView { game_id }
-            //     // };
-            //     let _ = nav.push(route);
-            // },
         }
     }
 }
